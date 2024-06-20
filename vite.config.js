5a4ed8fc0f6834e6c34ba0e9d9aa4dc9edc6f0c0
signflex <signflex.contact@gmail.com>
2024-06-20 16:43:09 +0100

    Configure i18n and static copy for translations

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,45 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   root : 'src',
+//   plugins: [
+//     react(),
+//     viteStaticCopy({
+//       targets: [
+//         {
+//           src: 'locales',
+//           dest: '.',
+//         },
+//       ],
+//     }),
+//   ],
+  
+//   build: {
+//     outDir: '../dist', // Output directory inside the src folder
+//     emptyOutDir: true, // Ensures the output directory is emptied before each build
+//   }
+// })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
 export default defineConfig({
-  root : 'src',
-  plugins: [react()],
+  root: 'src',
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'locales',
+          dest: '.',
+        },
+      ],
+    }),
+  ],
   build: {
-    outDir: '../dist', // Output directory inside the src folder
-    emptyOutDir: true, // Ensures the output directory is emptied before each build
+    outDir: '../dist',
+    emptyOutDir: true,
   }
-})
+});
